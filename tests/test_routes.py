16984f9f0b83d3c6c19992e59fc7632ea8e902f5
RayDeveloper <ray_5610@hotmail.co>
2024-09-04 15:03:16 -0400

    error-handlers added

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -163,3 +163,8 @@ class TestAccountService(TestCase):
         account.deserialize(request.get_json())
         account.update()
         return account.serialize(), status.HTTP_200_OK
+
+    def test_method_not_allowed(self):
+        """It should not allow an illegal method call"""
+        resp = self.client.delete(BASE_URL)
+        self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
